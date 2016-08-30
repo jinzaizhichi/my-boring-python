da0749af9f1ab05427eb1339e589c79ed84b39a9
LIZONGZE <919208140@qq.com>
2016-08-30 10:50:54 +0800

    fix time.py
    change vname and turn '1<2' into True

--- py/time.py
+++ py/time.py
@@ -1,11 +1,11 @@
 import datetime,time,os
 
-while 1<2 :
-	a = datetime.datetime.now()
-	b = datetime.datetime.strptime('2016-08-31  14:00:00', '%Y-%m-%d %H:%M:%S')
-	c = datetime.datetime.strptime('2016-09-15  00:00:00', '%Y-%m-%d %H:%M:%S')
-	d = datetime.datetime.strptime('2016-10-01  00:00:00', '%Y-%m-%d %H:%M:%S')
-	e = datetime.datetime.strptime('2017-01-01  00:00:00', '%Y-%m-%d %H:%M:%S')
+while True :
+	now_time = datetime.datetime.now()
+	summer_holiday = datetime.datetime.strptime('2016-08-31  14:00:00', '%Y-%m-%d %H:%M:%S')
+	mid_f = datetime.datetime.strptime('2016-09-15  00:00:00', '%Y-%m-%d %H:%M:%S')
+	country_f = datetime.datetime.strptime('2016-10-01  00:00:00', '%Y-%m-%d %H:%M:%S')
+	open_f = datetime.datetime.strptime('2017-01-01  00:00:00', '%Y-%m-%d %H:%M:%S')
 	print('距离暑假结束只剩下')
 	print (b-a)
 	print('距离16年中秋节还有')
