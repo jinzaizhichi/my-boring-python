fe9f0d2d3adcff7f64dcedab881216eb334f8329
c4oso_LZZ <lizongzeshunshun@gmail.com>
2016-08-30 11:16:52 +0800

    add wiki

---
+++ goWiki/README.md
@@ -0,0 +1,31 @@
+# goWiki
+
+[![GO-RP](https://goreportcard.com/badge/github.com/lizongzeshunshun/goWiki)](https://goreportcard.com/report/github.com/lizongzeshunshun/goWiki)
+
+## Introduction
+
+An open source wiki system using **golang**.
+
+## Version
+
+Now Version: **goWiki 0.1.1**
+
+## Usage
+
+**You can view the *main.go* and change the port and rebuild.**
+
+1. Use `http://127.0.0.1:<your_port(default: 8033)>/edit?filenm=<filename>` to create a new file or edit a file already in `templates/datafile`.
+2. You also can put your markdown file into `templates/datafile`.
+3. Use `http://127.0.0.1/arview?filenm=<filename>` to view a file.
+
+## Sample
+
+Sample: **http://article.daoapp.io/arview?filenm=test.md**
+
+## Donate
+
+Bitcoin Address: 1jcD1VcrMnma13P7JXhfAB6qaDkj5xhiJ
+
+Or you can scan this
+
+![Bitcoin_QR](https://github.com/lizongzeshunshun/goWiki/blob/master/BitQR.png)
---
+++ goWiki/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		logg("ERROR", err.Error())
+	}
+}
+
+func logg(logtype, logtext string) {
+	log.Printf("[%s] %s", logtype, logtext)
+}
+
+type Filer struct {
+	filename    string
+	filecontent string
+}
+
+func (f *Filer) checkExistsOrCreate() {
+	if checkFileExists("templates/datafile/"+f.filename) == false {
+		_, err := os.Create("templates/datafile/" + f.filename)
+		checkError(err)
+	}
+
+}
+
+func checkFileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+func (f *Filer) WriteToFile() {
+	binby := []byte(f.filecontent)
+	err := ioutil.WriteFile("templates/datafile/"+f.filename, binby, 0666)
+	checkError(err)
+}
+
+func (f *Filer) ReadFromFile() {
+	ff, err := os.Open("templates/datafile/" + f.filename)
+	checkError(err)
+	nnn, err := ioutil.ReadAll(ff)
+	checkError(err)
+	f.filecontent = string(nnn)
+}
+
+type Viewer struct {
+	Title   string
+	Content string
+}
+
+type Editer struct {
+	Filename string
+	Content  string
+}
+
+func editerHandler(w http.ResponseWriter, r *http.Request) {
+	var f Filer
+	f.filename = r.URL.Query()["filenm"][0]
+	f.checkExistsOrCreate()
+	f.ReadFromFile()
+	var edr Editer
+	edr.Filename = f.filename
+	edr.Content = f.filecontent
+	t, err := template.ParseFiles("templates/models/edit_model.html")
+	checkError(err)
+	t.Execute(w, &edr)
+
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	var f Filer
+	f.filecontent = r.PostFormValue("txx")
+	f.filename = r.PostFormValue("fn")
+	f.checkExistsOrCreate()
+	f.WriteToFile()
+}
+
+func viewerHandler(w http.ResponseWriter, r *http.Request) {
+	var f Filer
+	f.filename = r.URL.Query()["filenm"][0]
+	f.ReadFromFile()
+	var v Viewer
+	v.Title = f.filename
+	v.Content = f.filecontent
+	t, err := template.ParseFiles("templates/models/view_model.html")
+	checkError(err)
+	t.Execute(w, &v)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/models/index_model.html")
+	checkError(err)
+	t.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/arview", viewerHandler)
+	http.HandleFunc("/save_file", editHandler)
+	http.HandleFunc("/edit", editerHandler)
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/markdown_to_html/", http.FileServer(http.Dir("templates")))
+	http.ListenAndServe(":8033", nil)
+}
