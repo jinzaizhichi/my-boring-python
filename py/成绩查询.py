601e8fc1e33e66ba8f9fccdef4b75680391d8cee
redapple0204 <1229524609@qq.com>
2016-08-31 08:40:18 +0800

    Update 成绩查询.py

--- py/成绩查询.py
+++ py/成绩查询.py
@@ -1,43 +1,44 @@
+import time,random
+
 print('正在 从zph中获取数据')
 
 print('正在预测语文成绩')
 
-import time
+
 time.sleep(0.5)
-import random
+
 a = random.randint(45,100)
 print (a)
 print('正在 从nx中获取数据')
 print('正在预测数学成绩')
-import time
+
 time.sleep(0.5)
-import random
+
 b = random.randint(45,100)
 print (b)
 print('正在 从zhl中获取数据')
 print('正在预测英语成绩')
-import time
+
 time.sleep(0.5)
-import random
+
 c = random.randint(45,100)
 print (c)
 
 print('正在预测生物地理成绩')
-import time
+
 time.sleep(0.5)
-import random
+
 d = random.randint(45,100)
 print (d)
 
 print('正在预测历史成绩')
-import time
 time.sleep(0.5)
-import random
+
 e = random.randint(45,100)
 print (e)
 
 print('您的总分为')
-import time
+
 time.sleep(0.5)
 f = a+b+c+d+e
 print (f)
